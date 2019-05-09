0ebf219ee13b83edec79bb27a4c1b0c8e7487fe3
ColdMeekly <ant0711@my.londonmet.ac.uk>
2019-05-10 00:48:11 +0100

    Added AirTrafficPacket

--- Packets/AirTrafficPacket.py
+++ Packets/AirTrafficPacket.py
@@ -1,12 +1,31 @@
 import struct
+import io
+from CubeTypes import Airship
 class AirTrafficPacket():
+    pID = 0x3
     def __init__(self, airships):
         self.airships = airships
+        self.count = len(airships)
+
     @staticmethod
     def Import(data):
-        raise NotImplemented("Method must be implemented")
-    def Export(self):
-        raise NotImplemented("Method must be implemented")
-    def Send(self, sock):
-        raise NotImplemented("Method must be implemented")
+        dID, = struct.unpack('<I', data.read(4))
+        if (AirTrafficPacket.pID is not dID) :
+            raise ValueError(f'Received packet ID of {dID} when {AirTrafficPacket.pID} was expected.')
 
+        airshipsCount, = struct.unpack('<I', data.read(4))
+        airships = []
+        for _ in range(airshipsCount):
+            airships += Airship.Import(data.read(120))
+        
+        return AirTrafficPacket(airships)
+
+    def Export(self):
+        packet  = struct.pack('<I', pID)
+        packet += struct.pack('<I', self.count)
+        for ship in self.airships:
+            packet += ship.Export()
+        return packet
+
+    def Send(self, sock):
+        return sock.send(self.Export())
