9e70ce33afcb483a4f9848821bdc6746e4921bbd
ColdMeekly <ant0711@my.londonmet.ac.uk>
2019-05-09 17:52:58 +0100

    Added ChatPacket

---
+++ Packets/ChatPacket.py
@@ -0,0 +1,26 @@
+import struct
+import io
+class ChatPacket():
+    pID = 0xA
+    def __init__(self, message):
+        self.message = message
+    # Removed the 'self' parameter from Import because it's static
+    @staticmethod
+    def Import(data):
+        dID = struct.unpack('<I', data.read(4))
+        if (pID is not dID) :
+            raise ValueError(f'Received packet ID of {dID} when {pID} was expected.')
+
+        messageLength = struct.unpack('<I', data.read(4))
+        # Multiplying by 2 because each character uses 2 bytes
+        message  = data.read(messageLength*2).decode('utf-16le')
+        return ChatPacket(message)
+
+    def Export(self):
+        packet  = struct.pack('<I', pID)
+        packet += struct.pack('<I', len(self.message))
+        packet += self.message.encode('utf-16le')
+        return packet
+
+    def Send(self, sock):
+        return sock.send(self.Export())
