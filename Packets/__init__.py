c5e627ecf0d483b0cc81132f30b8cd36fe269f36
ChrisMiuchiz <chris000005@gmail.com>
2019-05-10 19:30:14 -0400

    Give easy access to packet classes

--- Packets/__init__.py
+++ Packets/__init__.py
@@ -4,4 +4,6 @@ from os.path import dirname, basename, isfile, join
 import glob
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and basename(f)[:-3] not in ('__init__', 'Packet')]
+from . import *
+classes = [getattr(globals()[x], x) for x in __all__]
 
