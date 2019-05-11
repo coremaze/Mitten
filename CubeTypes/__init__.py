faa99f26d22132a6a4f1b2cfdf98e6aea83526b9
ChrisMiuchiz <chris000005@gmail.com>
2019-05-11 17:03:22 -0400

    Add Item and Spirit to CubeTypes.__init__

--- CubeTypes/__init__.py
+++ CubeTypes/__init__.py
@@ -4,3 +4,5 @@ from .FloatVector3 import FloatVector3
 from .Creature import Creature
 from .Appearance import Appearance
 from .IntVector3 import IntVector3
+from .Item import Item
+from .Spirit import Spirit
