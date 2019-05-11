d688564fc84186ef851497cec987b62c25e12a08
ChrisMiuchiz <chris000005@gmail.com>
2019-05-10 20:19:29 -0400

    Add Connection

--- MITM.py
+++ MITM.py
@@ -6,13 +6,52 @@ PACKETS_CLASSES = Packets.classes
 INTERNAL_SERVER = ('localhost', 12344)
 EXTERNAL_SERVER = ('', 12345)
 
-def ClientHandler(clientSock, serverSock):
-    while True:
-        serverSock.send(clientSock.recv(1024))
-
-def ServerHandler(clientSock, serverSock):
-    while True:
-        clientSock.send(serverSock.recv(1024))
+class Connection():
+    def __init__(self, clientSock, serverSock):
+        self.clientSock = clientSock
+        self.serverSock = serverSock
+        self.closed = False
+        
+    def SendServer(self, data):
+        try: self.serverSock.send(data)
+        except: self.Close()
+        
+    def SendClient(self, data):
+        try: self.clientSock.send(data)
+        except: self.Close()
+        
+    def RecvServer(self, size):
+        buf = b''
+        try:
+            while len(buf) < size:
+                buf += self.serverSock.recv(size - len(buf))
+        except:
+            self.Close()
+        return buf
+    
+    def RecvClient(self, size):
+        buf = b''
+        try:
+            while len(buf) < size:
+                buf += self.clientSock.recv(size - len(buf))
+        except:
+            self.Close()
+        return buf
+    
+    def HandleClient(self):
+        while not self.closed:
+            self.SendServer(self.RecvClient(4))
+            
+    def HandleServer(self):
+        while not self.closed:
+            self.SendClient(self.RecvServer(4))
+            
+    def Close(self):
+        self.closed = True
+        try: self.serverSock.close()
+        except: pass
+        try: self.clientSock.close()
+        except: pass
 
 if __name__ == '__main__':
     listenSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,6 +65,8 @@ if __name__ == '__main__':
         serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serverSock.connect((INTERNAL_SERVER))
 
+        connection = Connection(clientSock, serverSock)
+
         #Create threads for each of these connections
-        Thread(target=ClientHandler, args=[clientSock, serverSock]).start()
-        Thread(target=ServerHandler, args=[clientSock, serverSock]).start()
+        Thread(target=Connection.HandleClient, args=[connection]).start()
+        Thread(target=Connection.HandleServer, args=[connection]).start()
