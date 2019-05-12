c5dd153f650c6305620860732a3465ff8e708df4
ChrisMiuchiz <chris000005@gmail.com>
2019-05-11 23:12:14 -0400

    Add RegionDiscoveredPacket

---
+++ Packets/RegionDiscoveredPacket.py
@@ -0,0 +1,27 @@
+import struct
+import io
+from .Packet import Packet
+class RegionDiscoveredPacket(Packet):
+    pID = 0xC
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    def Recv(connection, fromClient):
+        recv = [connection.RecvServer, connection.RecvClient][fromClient]
+
+        x,  = struct.unpack('<i', recv(4))
+        y, = struct.unpack('<i', recv(4))
+        return RegionDiscoveredPacket(x, y)
+
+    def Export(self, toServer):
+        packetByteList = []
+        packetByteList.append( struct.pack('<I', RegionDiscoveredPacket.pID) )
+        packetByteList.append( struct.pack('<i', self.x) )
+        packetByteList.append( struct.pack('<i', self.y) )
+        return b''.join(packetByteList)
+
+    def Send(self, connection, toServer):
+        send = [connection.SendClient, connection.SendServer][toServer]
+        return send(self.Export(toServer))
