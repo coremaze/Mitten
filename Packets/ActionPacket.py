d0e246ad31b61dc8fd9eabd208cf5ae5662c7795
ChrisMiuchiz <chris000005@gmail.com>
2019-05-12 17:26:11 -0400

    Split Interaction type in Action Packet

--- Packets/ActionPacket.py
+++ Packets/ActionPacket.py
@@ -4,13 +4,14 @@ from .Packet import Packet
 from CubeTypes.Item import Item
 class ActionPacket(Packet):
     pID = 0x6
-    def __init__(self, item, zoneX, zoneY, index, unknownInt, interactionType):
+    def __init__(self, item, zoneX, zoneY, index, unknownInt, interactionType, unkShort):
         self.item = item
         self.zoneX = zoneX
         self.zoneY = zoneY
         self.index = index
         self.unknownInt = unknownInt
         self.interactionType = interactionType
+        self.unkShort = unkShort
 
     @staticmethod
     def Recv(connection, fromClient):
@@ -21,9 +22,10 @@ class ActionPacket(Packet):
         zoneY, = struct.unpack('<i', recv(4))
         index, = struct.unpack('<i', recv(4))
         unknownInt, = struct.unpack('<i', recv(4))
-        interactionType, = struct.unpack('<i', recv(4))
+        interactionType, = struct.unpack('<H', recv(2))
+        unkShort, = struct.unpack('<H', recv(2))
 
-        return ActionPacket(item, zoneX, zoneY, index, unknownInt, interactionType)
+        return ActionPacket(item, zoneX, zoneY, index, unknownInt, interactionType, unkShort)
 
     def Export(self, toServer):
         packetByteList = []
@@ -33,7 +35,8 @@ class ActionPacket(Packet):
         packetByteList.append( struct.pack('<i', self.zoneY) )
         packetByteList.append( struct.pack('<i', self.index) )
         packetByteList.append( struct.pack('<i', self.unknownInt) )
-        packetByteList.append( struct.pack('<i', self.interactionType) )
+        packetByteList.append( struct.pack('<H', self.interactionType) )
+        packetByteList.append( struct.pack('<H', self.unkShort) )
         return b''.join(packetByteList)
 
     def Send(self, connection, toServer):
