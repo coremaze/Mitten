058eba039142291c5107630582ecf783f2a44cec
ChrisMiuchiz <chris000005@gmail.com>
2019-05-12 17:23:18 -0400

    Add ratelimiter plugin

---
+++ Plugins/RateLimiter.py
@@ -0,0 +1,60 @@
+from Packets.JoinPacket import JoinPacket
+from Packets.ChatPacket import ChatPacket
+from Packets.ActionPacket import ActionPacket
+import time
+
+action_packet_stacks = {}
+
+def BanIP(IP):
+    while True:
+        try:
+            with open('bans.txt', 'w+') as f:
+                f.write(f'\n{IP}\n')
+        except FileNotFoundError:
+            with open('bans.txt', 'w') as f:
+                pass
+        else:
+            break
+
+def HandlePacket(connection, packet, fromClient):
+
+    #Remove old records
+    to_close = []
+    for conn in action_packet_stacks:
+        if conn.closed:
+            to_close.append(conn)
+    for conn in to_close:
+        del action_packet_stacks[conn]
+
+        
+    if type(packet) == JoinPacket:
+        return HandleJoinPacket(connection, packet, fromClient)
+    if type(packet) == ActionPacket:
+        return HandleActionPacket(connection, packet, fromClient)
+
+
+
+def HandleJoinPacket(connection, packet, fromClient):
+    action_packet_stacks[connection] = []
+    
+def HandleActionPacket(connection, packet, fromClient):
+    curTime = time.time()
+    action_packet_stacks[connection].append(curTime)
+    if len(action_packet_stacks[connection]) > 500:
+        action_packet_stacks[connection].pop(0)
+
+    #last 6 seconds
+    recentActions = sum([1 for x in action_packet_stacks[connection] if (curTime - x) < 6])
+    print(f'{recentActions} recent actions')
+
+    #last 60 seconds
+    lessRecentActions = sum([1 for x in action_packet_stacks[connection] if (curTime - x) < 60])
+    print(f'{lessRecentActions} less recent actions')
+
+    
+    if recentActions >= 100 or lessRecentActions >= 420:
+        IP = connection.clientSock.getsockname()[0]
+        BanIP(IP)
+        print(f'Banning {IP}. They sent {recentActions} actions in the last 6 seconds, and {lessRecentActions} actions in the last 60 seconds.')
+        connection.Close()
+        return True
