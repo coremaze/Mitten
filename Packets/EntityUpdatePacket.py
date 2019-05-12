c76badb5f18031e6b5dc75bc126cd78dbb16d8a1
ChrisMiuchiz <chris000005@gmail.com>
2019-05-11 22:54:23 -0400

    add EntityUpdatePacket. Doesn't yet parse zlib data

---
+++ Packets/EntityUpdatePacket.py
@@ -0,0 +1,28 @@
+import struct
+import io
+from .Packet import Packet
+import zlib
+class EntityUpdatePacket(Packet):
+    pID = 0x0
+    def __init__(self, zlibData):
+        self.zlibData = zlibData #let's actually parse this eventually
+
+    @staticmethod
+    def Recv(connection, fromClient):
+        recv = [connection.RecvServer, connection.RecvClient][fromClient]
+        
+        size, = struct.unpack('<I', recv(4))
+        zlibData, = recv(size)
+            
+        return EntityUpdatePacket(zlibData)
+
+    def Export(self, toServer):
+        packetByteList = []
+        packetByteList.append( struct.pack('<I', JoinPacket.pID) )
+        packetByteList.append( struct.pack('<I', len(self.zlibData)) )
+        packetByteList.append( self.zlibData )
+        return b''.join(packetByteList)
+
+    def Send(self, connection, toServer):
+        send = [connection.SendClient, connection.SendServer][toServer]
+        return send(self.Export(toServer))
