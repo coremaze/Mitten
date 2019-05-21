6418c990c3c8b12bf7266f1f99876df20f7e1c00
ChrisMiuchiz <chris000005@gmail.com>
2019-05-20 21:50:25 -0400

    Add configs

---
+++ Mitten/Configs.py
@@ -0,0 +1,52 @@
+import json
+
+CONFIG = {}
+
+def LoadConfig():
+    global CONFIG
+    while True:
+        try:
+            with open('config.json', 'r') as f:
+                data = f.read().strip()
+        except FileNotFoundError:
+            with open('config.json', 'w') as f:
+                pass
+        else:
+            break
+    if not data:
+        data = '{}'
+    CONFIG = json.loads(data)
+    
+
+def SaveConfig():
+    while True:
+        try:
+            with open('config.json', 'w') as f:
+                f.write(json.dumps(CONFIG, indent=4))
+        except: pass
+        else: break
+        
+def PluginName(plugin):
+    if type(plugin) != str:
+        string = plugin.__name__.split('.')[0]
+    else:
+        string = plugin
+    return string
+
+def LoadPlugin(plugin):
+    string = PluginName(plugin)
+    if string not in CONFIG:
+        CONFIG[string] = {'enabled': True}
+    SaveConfig()
+
+def GetAttribute(plugin, attribute, default=None):
+    string = PluginName(plugin)
+    if attribute not in CONFIG[string]:
+        CONFIG[string][attribute] = default
+    return CONFIG[string][attribute]
+
+def SetAttribute(plugin, attribute, value):
+    string = PluginName(plugin)
+    CONFIG[attribute] = value
+    SaveConfig()
+    
--- Plugins/__init__.py
+++ Plugins/__init__.py
@@ -2,7 +2,19 @@
 
 from os.path import dirname, basename, isfile, join
 import glob
+import Mitten.Configs
+
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and basename(f)[:-3] not in ('__init__', 'Packet')]
-from . import *
-pluginList = [globals()[x] for x in __all__]
+
+#Load configs for plugins, and the plugins which are enabled.
+Mitten.Configs.LoadConfig()
+pluginList = []
+for pluginName in __all__:
+    Mitten.Configs.LoadPlugin(pluginName)
+    if Mitten.Configs.GetAttribute(pluginName, 'enabled'):
+        print(f'Loading plugin {pluginName}')
+        __import__('Plugins.'+pluginName)
+        pluginList.append(globals()[pluginName])
+    else:
+        print(f'NOT loading plugin {pluginName}')
