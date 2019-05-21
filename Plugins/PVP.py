b9762e540671f36b5a1c6a1a0e03c8fc529ab79d
ChrisMiuchiz <chris000005@gmail.com>
2019-05-21 17:19:47 -0400

    hopefully this is agressive enough to prevent friendly fire

--- Plugins/PVP.py
+++ Plugins/PVP.py
@@ -3,6 +3,7 @@ from Mitten.Constants import *
 from Packets.EntityUpdatePacket import EntityUpdatePacket
 from Packets.JoinPacket import JoinPacket
 from Packets.ChatPacket import ChatPacket
+from Packets.HitPacket import HitPacket
 from threading import Thread
 
 PLAYERS = {}
@@ -42,6 +43,8 @@ def HandlePacket(connection, packet, fromClient):
         return HandleJoin(connection, packet, fromClient)
     if type(packet) == EntityUpdatePacket:
         return HandleEntityUpdate(connection, packet, fromClient)
+    if type(packet) == HitPacket:
+        return HandleHit(connection, packet, fromClient)
 
 
 def HandleJoin(connection, packet, fromClient):
@@ -75,5 +78,34 @@ def HandleEntityUpdate(connection, packet, fromClient):
         h2 = PLAYERS[connection]['team'] != PLAYERS[otherConn]['team']
         packet.fields['hostility'] = h1 and h2
         return MODIFY
-    
-    
+
+def HandleHit(connection, packet, fromClient):
+    if not fromClient: return NO_ACTION
+    #Block anyone who doesn't have an ID yet
+    if not PLAYERS[connection]['ID']: return BLOCK
+    #Block if the player is claiming they're an entity they're not
+    if PLAYERS[connection]['ID'] != packet.attackerID: return BLOCK
+    #Allow if the target is not a player
+    if packet.targetID not in [x['ID'] for x in PLAYERS.values()]: return NO_ACTION
+
+    try:
+        otherPlayer = PLAYERS[GetConnectionByID(packet.targetID)]
+        thisPlayer = PLAYERS[connection]
+    except KeyError:
+        return BLOCK
+
+    if packet.dmg >= 0:
+        #Block if either are not on a team and attacking
+        if otherPlayer['team'] is None or thisPlayer['team'] is None: return BLOCK
+
+        #Block if they are on same team and attacking
+        if otherPlayer['team'] == thisPlayer['team']: return BLOCK
+    else:
+        #Allow healing if the recipient is on no team
+        if otherPlayer['team'] is None: return NO_ACTION
+        
+        #Block if they are on different teams and healing
+        if otherPlayer['team'] != thisPlayer['team']: return BLOCK
+
+
+    return NO_ACTION
