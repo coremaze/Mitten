4ca6abc3962a6c88ae6467f74b39718893fe5898
ChrisMiuchiz <chris000005@gmail.com>
2019-05-22 22:10:58 -0400

    Readd forward handling

--- Mitten/Events.py
+++ Mitten/Events.py
@@ -5,17 +5,21 @@ class OnPacket():
     connection: The Connection receiving the packet
     packet: The Packet which has been received
     fromClient: A bool representing the direction the packet is coming from
+
+    return NO_ACTION or None to have Mitten continue forwarding as normal
+    return BLOCK to have Mitten not continue processing the packet
+    return MODIFY to have Mitten re-export the packet
     '''
     pass
 class OnServerFailure():
     '''
-    Triggered the server refuses a connection.
+    Triggered when the server refuses a connection.
     '''
     pass
 
 class OnConnect():
     '''
-    Triggered the server refuses a connection.
+    Triggered when a connection is made.
 
     connection: The new Connection
     '''
@@ -23,17 +27,31 @@ class OnConnect():
 
 class OnDisconnect():
     '''
-    Triggered the server refuses a connection.
+    Triggered when a connection is broken.
 
     connection: The departing Connection
     '''
     pass
 
+class OnForward():
+    '''
+    Triggered when a connection to the internal server is about to be made.
+
+    clientSock: The socket of the incoming client
+    address: A string containing the IP address of the incoming client
+    port: An int representing the port of the incoming client
+
+    return NO_ACTION or None to have Mitten connect to the internal server as normal
+    return BLOCK to have Mitten stop processing the connection. You are responsible for closing the client socket, if you need to.
+    return a tuple with ('Server address' portNumber) to have Mitten forward to a different server.
+    '''
+
 MITTEN_EVENTS = {
     OnPacket: [],
     OnServerFailure: [],
     OnConnect: [],
-    OnDisconnect: []
+    OnDisconnect: [],
+    OnForward: []
     }
 
 def Handle(eventType):
