125d93c6f6b26cabb012b0e144585735b857b1ba
ChrisMiuchiz <chris000005@gmail.com>
2019-05-26 15:51:56 -0400

    Reduce default range on noboomies

--- Plugins/NoBoomies.py
+++ Plugins/NoBoomies.py
@@ -5,7 +5,7 @@ from Packets import *
 from CubeTypes import *
 
 PLUGIN = __name__.split('.')[-1]
-maxBlocksAway = Configs.GetAttribute(PLUGIN, 'maxBlocksAway', (ZONE_SCALE // BLOCK_SCALE) * 2) #Default is 2 zones away
+maxBlocksAway = Configs.GetAttribute(PLUGIN, 'maxBlocksAway', (ZONE_SCALE // BLOCK_SCALE) * 1) #Default is 1 zone1 away
 
 class Player:
     def __init__(self, connection):
