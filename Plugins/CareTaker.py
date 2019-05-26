0c41540cb16cca837279a06f818a28234bfe7f7d
ColdMeekly <ant0711@my.londonmet.ac.uk>
2019-05-26 03:52:58 +0100

    Added new plugin - CareTaker

---
+++ Plugins/CareTaker.py
@@ -0,0 +1,69 @@
+import os
+import psutil
+from Mitten.Events import *
+from Mitten import Configs
+
+PLUGIN = __name__.split('.')[-1]
+
+class CareTaker():
+    def __init__(self, server, processName, path):
+        self.host, self.port = server
+        self.processName = processName
+        self.path = path
+        self.exe = self.path + self.processName
+        self._processName = 'Server.exe'
+        self._exe = self.path + self._processName
+
+    # Check output of this func to make sure it's not None
+    def _GetRunningServer(self):
+        try:
+            for process in psutil.process_iter():
+                if process.name() == self._processName:
+                    if process.exe() == self._exe:
+                        return process
+        except Exception as e:
+            print(f'[{PLUGIN}] Error in _GetRunningServer!'
+                  f'* Error: {e}')
+        return None
+
+    def _StartServer(self):
+        try:
+            os.chdir(self.path)
+            os.startfile(self.exe)
+        except Exception as e:
+            print(f'[{PLUGIN}] Could not start server!'
+                  f'* In directory: {self.path}'
+                  f'* Process: {self.processName}'
+                  f'* Error: {e}')
+            return
+        print(f'[{PLUGIN}] Successfully started {self.processName}')
+
+    def _CloseServer(self):
+        serverProcess = self._GetRunningServer()
+        if serverProcess is None: return
+        try:
+            serverProcess.kill()
+        except Exception as e:
+            print(f'[{PLUGIN}] Could not kill server!'
+                  f'* Error: {e}')
+
+    def RestartServer(self):
+        self._CloseServer()
+        self._StartServer()
+
+
+# TODO: Instead of using hardcoded value, fetch from global config
+# Initialize the caretaker :)
+caretaker = CareTaker(
+    ('localhost', 12344),
+    Configs.GetAttribute(PLUGIN, 'internalExecutableName', 'Server.exe'),
+    Configs.GetAttribute(PLUGIN, 'internalExecutablePath', os.path.abspath(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+    )) + '\\\\'
+)
+
+@Handle(OnServerFailure)
+def FailureHandler(sock, server):
+    print(f'[{PLUGIN}] Handling a server failure :o')
+    caretaker.RestartServer()
+    sock.connect(server)
